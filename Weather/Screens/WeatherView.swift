eb3c6472d6a2a9920ee86f057bd7871470758594
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-09-09 21:48:31 -0400

    Project organization, created the initial WeatherView

---
+++ Weather/Screens/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Deonte Kilgore on 9/9/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("Weather Here")
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
